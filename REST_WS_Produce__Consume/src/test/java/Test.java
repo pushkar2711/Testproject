450a0e411ef1d974f65751bd50652008e8430fe3
Your Name <pushkraj8999@gmail.com>
2018-04-05 22:44:30 +0530

    change1

--- REST_WS_Produce__Consume/src/test/java/Test.java
+++ REST_WS_Produce__Consume/src/test/java/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hii");
-		System.out.println("i am raj rathod");
+		System.out.println("i am raj rathod A1");
                 
                 System.out.println("i am raj1 rathod");
 	}
