d8dee2dd3ffd2864e75701f2d8da33b7a2b79f62
pushkraj jagtap <pushkraj8999@gmail.com>
2018-04-07 20:48:01 +0530

    new change

--- REST_WS_Produce__Consume/src/test/java/Test.java
+++ REST_WS_Produce__Consume/src/test/java/Test.java
@@ -2,9 +2,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hii");
-		System.out.println("i am raj rathod A1");
-                
-                System.out.println("i am raj1 rathod");
+		
 	}
 
 }
